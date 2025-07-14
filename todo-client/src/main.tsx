e72dd74fa976c899db86813fd18b635fb4869b2f
ueda <adachi@ueda78.com>
2025-07-14 09:51:29 +0100

    フロントのコンソールから返り値を見られるようにする

--- todo-client/src/main.tsx
+++ todo-client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, type ApolloQueryResult } from '@apollo/client';
 
 const cachetype = new InMemoryCache(
   // どのクエリーに対してキャッシュを効かせるか
@@ -12,6 +12,20 @@ const apolloclient = new ApolloClient( {
   uri: "http://localhost:4000", // graphql-serveで作ったapollo-server
   cache: cachetype,
 } );
+
+apolloclient.query( {
+  query: gql`
+    query {
+      getTodos {
+        id
+        title
+        completed
+      }
+    }
+    `,
+} ).then( ( r: ApolloQueryResult<any> ): void => console.log( `r`, r ) );
+
+
 
 createRoot( document.getElementById( 'root' )! ).render(
   <StrictMode>
